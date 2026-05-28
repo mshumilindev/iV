bfde609b7921b9941b6dd3a74afd188a0ce0c1ca
Nikolay Lukyanov <Nikolay.Lukyanov@dataart.com>
2026-05-28 16:45:53 +0200

    Initial Commit

---
+++ iV/iVApp.swift
@@ -0,0 +1,17 @@
+//
+//  iVApp.swift
+//  iV
+//
+//  Created by Mykola Lukianov on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct iVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iVTests/iVTests.swift
@@ -0,0 +1,18 @@
+//
+//  iVTests.swift
+//  iVTests
+//
+//  Created by Mykola Lukianov on 28.05.2026.
+//
+
+import Testing
+
+struct iVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
